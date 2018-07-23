184d7e42d4de025515a3dd2f2f1ac7239c4ed4d5
Wenyuan Ma <wm263@cornell.edu>
2018-07-23 15:29:10 -0400

    add ebike data

--- ebike_startJFK.py
+++ ebike_startJFK.py
@@ -2,7 +2,7 @@ import csv
 import time
 import datetime
 import collections
-from Dijkstra import shortest_path
+from Dijkstra import shortest_path, dijsktra
 from build_network import build_graph, modify_edges
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,15 +32,22 @@ with open('data/bikeStation_coordinates.csv') as f:
         bike_ids.add('bike_' + row[0])
         
 graph = build_graph()
+dest = set()
+for (id1, id2), ti in taxi_time.items():
+    if id1=='132':
+        dest.add(id2)
 new_multi_time = {}
 original_graph = deepcopy(graph)
+import glob
+exist = set(glob.glob("data/ebike/*.csv"))
 for b_id in bike_ids:
     modify_edges(graph, b_id)
-    with open('data/ebike/'+b_id+'.csv','w') as file:
-        for (id1, id2), ti in taxi_time.items():
-            if id1=='132':
-                new_multi_time[id1,id2], path, route_type = shortest_path(graph,id1,id2)
-                print(id1,id2,new_multi_time[id1,id2])
-                file.write(id1 + ',' + id2 + ',' +
-                           str(new_multi_time[id1,id2]) + ',' + str(taxi_time[id1,id2]) + '\n')
+    filename = 'data/ebike/' + b_id + '.csv'
+    if filename not in exist:
+        with open(filename,'w') as file:
+            dist, path, route = dijsktra(graph,'132')
+            for id2 in dest:
+                print(b_id,'132',id2,dist[id2])
+                file.write('132' + ',' + id2 + ',' +
+                            str(dist[id2]) + ',' + str(taxi_time['132',id2]) + '\n')
     graph = original_graph
